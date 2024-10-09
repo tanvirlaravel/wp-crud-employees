c773958d630f288e9c43ec8120edc788111804da
Tanvir <tanvirlaravel@gmail.com>
2024-10-09 11:49:19 +0600

    initial commit

---
+++ wp-crud-employees.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+* Plugin Name: WP Crud Employees
+* Plugin URI:  https://www.wp-crud-employees.com
+* Description: CSV Data Uploader
+* Version:     1.0.0
+* Author:      Tanvir
+* Author URI:  https://github.com/tanvirlaravel
+* License:     GPL2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: wp-crud-employees
+*/
+
+
+if(!defined("ABSPATH")){
+    exit;
+}
